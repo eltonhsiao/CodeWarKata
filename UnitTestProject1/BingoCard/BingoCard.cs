02c69247b0ba2565231a9b92358fcaa562936eae
Elton <st10426@hotmail.com>
2017-12-08 23:42:20 +0800

    add NumbersWithinColumnAreAllInTheCorrectRange test and passed

--- UnitTestProject1/BingoCard/BingoCard.cs
+++ UnitTestProject1/BingoCard/BingoCard.cs
@@ -11,11 +11,11 @@ namespace CodeWarKata
     {
         public static string[] GetCard()
         {
-            var BNumbers = NumberGenerator(count: 5);
-            var INumbers = NumberGenerator(count: 5);
-            var NNumbers = NumberGenerator(count: 4);
-            var GNumbers = NumberGenerator(count: 5);
-            var ONumbers = NumberGenerator(count: 5);
+            var BNumbers = NumberGenerator(start: 1, end: 15, count: 5);
+            var INumbers = NumberGenerator(start: 16, end: 30, count: 5);
+            var NNumbers = NumberGenerator(start: 31, end: 45, count: 4);
+            var GNumbers = NumberGenerator(start: 46, end: 60, count: 5);
+            var ONumbers = NumberGenerator(start: 61, end: 75, count: 5);
             return BNumbers.Select(x => "B" + x.ToString()).Concat(
                    INumbers.Select(x => "I" + x.ToString()).Concat(
                    NNumbers.Select(x => "N" + x.ToString()).Concat(
@@ -23,13 +23,13 @@ namespace CodeWarKata
                    ONumbers.Select(x => "O" + x.ToString()))))).ToArray();
         }
 
-        private static List<int> NumberGenerator(int count)
+        private static List<int> NumberGenerator(int start, int end, int count)
         {
             var numbers = new List<int>();
             var random = new Random();
             while (numbers.Count < count)
             {
-                var number = random.Next(1, 15);
+                var number = random.Next(start, end);
                 if (!numbers.Contains(number))
                     numbers.Add(number);
             }
