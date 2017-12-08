6283e88b16e3b740884b99893cd2d91abfefbe3c
Elton <st10426@hotmail.com>
2017-12-08 23:51:21 +0800

    add NumbersWithinColumnAreInRandomOrder and pass test

--- UnitTestProject1/BingoCard/BingoCard.cs
+++ UnitTestProject1/BingoCard/BingoCard.cs
@@ -11,11 +11,11 @@ namespace CodeWarKata
     {
         public static string[] GetCard()
         {
-            var BNumbers = NumberGenerator(start: 1, end: 15, count: 5);
-            var INumbers = NumberGenerator(start: 16, end: 30, count: 5);
-            var NNumbers = NumberGenerator(start: 31, end: 45, count: 4);
-            var GNumbers = NumberGenerator(start: 46, end: 60, count: 5);
-            var ONumbers = NumberGenerator(start: 61, end: 75, count: 5);
+            var BNumbers = BingoNumberGenerator(start: 1, end: 15, count: 5);
+            var INumbers = BingoNumberGenerator(start: 16, end: 30, count: 5);
+            var NNumbers = BingoNumberGenerator(start: 31, end: 45, count: 4);
+            var GNumbers = BingoNumberGenerator(start: 46, end: 60, count: 5);
+            var ONumbers = BingoNumberGenerator(start: 61, end: 75, count: 5);
             return BNumbers.Select(x => "B" + x.ToString()).Concat(
                    INumbers.Select(x => "I" + x.ToString()).Concat(
                    NNumbers.Select(x => "N" + x.ToString()).Concat(
@@ -23,7 +23,7 @@ namespace CodeWarKata
                    ONumbers.Select(x => "O" + x.ToString()))))).ToArray();
         }
 
-        private static List<int> NumberGenerator(int start, int end, int count)
+        private static List<int> BingoNumberGenerator(int start, int end, int count)
         {
             var numbers = new List<int>();
             var random = new Random();
