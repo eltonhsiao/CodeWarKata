f1b8fbe7bea57bf31635c09db52e728ed8d860ee
Elton <st10426@hotmail.com>
2017-12-09 23:47:05 +0800

    extract mapping dictionary to new class

---
+++ MorseCodeDecoder/MorseCode.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeDecoder
+{
+    class MorseCode
+    {
+        private static Dictionary<string, string> MorseCodesLettersAndNumerals = new Dictionary<string, string>()
+        {
+            {".-", "A"},
+            {"-...", "B"},
+            {"-.-.", "C"},
+            {"-..", "D"},
+            {".", "E"},
+            {"..-.", "F"},
+            {"--.", "G"},
+            {"....", "H"},
+            {"..", "I"},
+            {".---", "J"},
+            {"-.-", "K"},
+            {".-..", "L"},
+            {"--", "M"},
+            {"-.", "N"},
+            {"---", "O"},
+            {".--.", "P"},
+            {"--.-", "Q"},
+            {".-.", "R"},
+            {"...", "S"},
+            {"-", "T"},
+            {"..-", "U"},
+            {"...-", "V"},
+            {".--", "W"},
+            {"-..-", "X"},
+            {"-.--", "Y"},
+            {"--..", "Z"},
+            {"-----", "0"},
+            {".----", "1"},
+            {"..---", "2"},
+            {"...--", "3"},
+            {"....-", "4"},
+            {".....", "5"},
+            {"-....", "6"},
+            {"--...", "7"},
+            {"---..", "8"},
+            {"----.", "9"},
+            {"...---...", "SOS" },
+            {"-.-.--", "!"},
+            {".-.-.-", "." }
+        };
+
+        public static string Get(string code)
+        {
+            return MorseCodesLettersAndNumerals[code];
+        }
+    }
+}
--- MorseCodeDecoder/MorseCodeDecoder.cs
+++ MorseCodeDecoder/MorseCodeDecoder.cs
@@ -1,56 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace MorseCodeDecoder
 {
     public class MorseCodeDecoder
     {
-        private static Dictionary<string, string> MorseCodesLettersAndNumerals = new Dictionary<string, string>()
-        {
-            {".-", "A"},
-            {"-...", "B"},
-            {"-.-.", "C"},
-            {"-..", "D"},
-            {".", "E"},
-            {"..-.", "F"},
-            {"--.", "G"},
-            {"....", "H"},
-            {"..", "I"},
-            {".---", "J"},
-            {"-.-", "K"},
-            {".-..", "L"},
-            {"--", "M"},
-            {"-.", "N"},
-            {"---", "O"},
-            {".--.", "P"},
-            {"--.-", "Q"},
-            {".-.", "R"},
-            {"...", "S"},
-            {"-", "T"},
-            {"..-", "U"},
-            {"...-", "V"},
-            {".--", "W"},
-            {"-..-", "X"},
-            {"-.--", "Y"},
-            {"--..", "Z"},
-            {"-----", "0"},
-            {".----", "1"},
-            {"..---", "2"},
-            {"...--", "3"},
-            {"....-", "4"},
-            {".....", "5"},
-            {"-....", "6"},
-            {"--...", "7"},
-            {"---..", "8"},
-            {"----.", "9"},
-            {"...---...", "SOS" },
-            {"-.-.--", "!"},
-            {".-.-.-", "." }
-        };
+        
 
         public static string Decode(string morseCode)
         {
@@ -60,7 +15,7 @@ namespace MorseCodeDecoder
             {
                 foreach (var morse in word.Split(' '))
                 {
-                    pureText += MorseCodesLettersAndNumerals[morse];
+                    pureText += MorseCode.Get(morse);
                 }
 
                 pureText += " ";
