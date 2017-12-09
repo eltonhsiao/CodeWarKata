cdf4344444a9cc2b676a210b3f18378406b6cda8
Elton <st10426@hotmail.com>
2017-12-09 23:36:48 +0800

    add third test case and add mark into dictionary

--- MorseCodeDecoder/MorseCodeDecoderTests.cs
+++ MorseCodeDecoder/MorseCodeDecoderTests.cs
@@ -41,5 +41,41 @@ namespace MorseCodeDecoder
                 Assert.Fail("There seems to be an error somewhere in your code. Exception message reads as follows: " + ex.Message);
             }
         }
+
+        [Test]
+        public void MorseCodeDecoderBasicTest_6()
+        {
+            try
+            {
+                string input = "...---...";
+                string expected = "SOS";
+
+                string actual = MorseCodeDecoder.Decode(input);
+
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("There seems to be an error somewhere in your code. Exception message reads as follows: " + ex.Message);
+            }
+        }
+
+        [Test]
+        public void MorseCodeDecoderComplexTest_3()
+        {
+            try
+            {
+                string input = "      ...---... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.-  ";
+                string expected = "SOS! THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+
+                string actual = MorseCodeDecoder.Decode(input);
+
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("There seems to be an error somewhere in your code. Exception message reads as follows: " + ex.Message);
+            }
+        }
     }
 }
