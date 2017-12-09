6cf3eaaa367614d7e1d9a7d4f4b2a8e15a1a0677
Elton <st10426@hotmail.com>
2017-12-09 23:12:39 +0800

    add first test and pass

--- MorseCodeDecoder/MorseCodeDecoder.cs
+++ MorseCodeDecoder/MorseCodeDecoder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MorseCodeDecoder
@@ -10,7 +11,29 @@ namespace MorseCodeDecoder
     {
         public static string Decode(string morseCode)
         {
-            throw new System.NotImplementedException("Please provide some code.");
+            var morseCodesLettersAndNumerals = new Dictionary<string, string>()
+            {
+                {"....", "H"},
+                {".", "E"},
+                {"-.--", "Y"},
+                {".---", "J"},
+                {"..-", "U"},
+                {"-..", "D"}
+            };
+
+            var words = Regex.Split(morseCode, @"\s{2,}");
+            string pureText = string.Empty;
+            foreach (var word in words)
+            {
+                foreach (var morse in word.Split(' '))
+                {
+                    pureText += morseCodesLettersAndNumerals[morse];
+                }
+                
+                pureText += " ";
+            }
+            
+            return pureText.Trim();
         }
     }
 }
