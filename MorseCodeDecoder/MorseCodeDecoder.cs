3cb817579393c53ac2ff5232a57b30c050a539ba
Elton <st10426@hotmail.com>
2017-12-09 22:38:29 +0800

    create MorseCodeDecoder project

---
+++ MorseCodeDecoder/MorseCodeDecoder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeDecoder
+{
+    public class MorseCodeDecoder
+    {
+        public static string Decode(string morseCode)
+        {
+            throw new System.NotImplementedException("Please provide some code.");
+        }
+    }
+}
---
+++ MorseCodeDecoder/MorseCodeDecoderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MorseCodeDecoder
+{
+    [TestFixture]
+    class MorseCodeDecoderTests
+    {
+        [Test]
+        public void MorseCodeDecoderBasicTest_1()
+        {
+            try
+            {
+                string input = ".... . -.--   .--- ..- -.. .";
+                string expected = "HEY JUDE";
+
+                string actual = MorseCodeDecoder.Decode(input);
+
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("There seems to be an error somewhere in your code. Exception message reads as follows: " + ex.Message);
+            }
+        }
+    }
+}
