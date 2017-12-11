0a0aa68b311a5d806b4f4e62d76e74bba4937d8a
Elton <st10426@hotmail.com>
2017-12-11 18:07:19 +0800

    diff method, try change return to yield return

--- BingoCard/BingoCard.cs
+++ BingoCard/BingoCard.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace BingoCard
 {
@@ -8,20 +7,17 @@ namespace BingoCard
     {
         public static string[] GetCard()
         {
-            var BNumbers = BingoNumberGenerator(start: 1, end: 16, count: 5);
-            var INumbers = BingoNumberGenerator(start: 16, end: 31, count: 5);
-            var NNumbers = BingoNumberGenerator(start: 31, end: 46, count: 4);
-            var GNumbers = BingoNumberGenerator(start: 46, end: 61, count: 5);
-            var ONumbers = BingoNumberGenerator(start: 61, end: 76, count: 5);
+            var bingoCard = new List<string>();
+            bingoCard.AddRange(BingoNumberGenerator(column: "B", start: 1, end: 16, count: 5));
+            bingoCard.AddRange(BingoNumberGenerator(column: "I", start: 16, end: 31, count: 5));
+            bingoCard.AddRange(BingoNumberGenerator(column: "N", start: 31, end: 46, count: 4));
+            bingoCard.AddRange(BingoNumberGenerator(column: "G", start: 46, end: 61, count: 5));
+            bingoCard.AddRange(BingoNumberGenerator(column: "O", start: 61, end: 76, count: 5));
 
-            return BNumbers.Select(x => "B" + x.ToString()).Concat(
-                   INumbers.Select(x => "I" + x.ToString()).Concat(
-                   NNumbers.Select(x => "N" + x.ToString()).Concat(
-                   GNumbers.Select(x => "G" + x.ToString()).Concat(
-                   ONumbers.Select(x => "O" + x.ToString()))))).ToArray();
+            return bingoCard.ToArray();
         }
 
-        private static List<int> BingoNumberGenerator(int start, int end, int count)
+        private static IEnumerable<string> BingoNumberGenerator(string column, int start, int end, int count)
         {
             var randomNumbers = new List<int>();
             var random = new Random();
@@ -29,10 +25,11 @@ namespace BingoCard
             {
                 var number = random.Next(start, end);
                 if (!randomNumbers.Contains(number))
+                {
                     randomNumbers.Add(number);
+                    yield return column + number;
+                }
             }
-
-            return randomNumbers;
         }
     }
 }
