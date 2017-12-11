8d5eaf25e87180bed95971d58552e91bda73f777
Elton <st10426@hotmail.com>
2017-12-11 17:27:33 +0800

    fix random number generate

--- BingoCard/BingoCard.cs
+++ BingoCard/BingoCard.cs
@@ -8,30 +8,31 @@ namespace BingoCard
     {
         public static string[] GetCard()
         {
-            var BNumbers = BingoNumberGenerator(start: 1, end: 15, count: 5);
-            var INumbers = BingoNumberGenerator(start: 16, end: 30, count: 5);
-            var NNumbers = BingoNumberGenerator(start: 31, end: 45, count: 4);
-            var GNumbers = BingoNumberGenerator(start: 46, end: 60, count: 5);
-            var ONumbers = BingoNumberGenerator(start: 61, end: 75, count: 5);
+            var BNumbers = BingoNumberGenerator(start: 1, end: 16, count: 5);
+            var INumbers = BingoNumberGenerator(start: 16, end: 31, count: 5);
+            var NNumbers = BingoNumberGenerator(start: 31, end: 46, count: 4);
+            var GNumbers = BingoNumberGenerator(start: 46, end: 61, count: 5);
+            var ONumbers = BingoNumberGenerator(start: 61, end: 76, count: 5);
+
             return BNumbers.Select(x => "B" + x.ToString()).Concat(
-                INumbers.Select(x => "I" + x.ToString()).Concat(
-                    NNumbers.Select(x => "N" + x.ToString()).Concat(
-                        GNumbers.Select(x => "G" + x.ToString()).Concat(
-                            ONumbers.Select(x => "O" + x.ToString()))))).ToArray();
+                   INumbers.Select(x => "I" + x.ToString()).Concat(
+                   NNumbers.Select(x => "N" + x.ToString()).Concat(
+                   GNumbers.Select(x => "G" + x.ToString()).Concat(
+                   ONumbers.Select(x => "O" + x.ToString()))))).ToArray();
         }
 
         private static List<int> BingoNumberGenerator(int start, int end, int count)
         {
-            var numbers = new List<int>();
+            var randomNumbers = new List<int>();
             var random = new Random();
-            while (numbers.Count < count)
+            while (randomNumbers.Count < count)
             {
                 var number = random.Next(start, end);
-                if (!numbers.Contains(number))
-                    numbers.Add(number);
+                if (!randomNumbers.Contains(number))
+                    randomNumbers.Add(number);
             }
 
-            return numbers;
+            return randomNumbers;
         }
     }
 }
